6fb28c950d5902e2ad491a249e7d4cf24a0ac6dc
Medhat Hassan <105995852+MedhatHassan@users.noreply.github.com>
2023-05-24 02:20:32 +0300

    Add files via upload

--- Keylogger/SRC/device.java
+++ Keylogger/SRC/device.java
@@ -1,3 +1,19 @@
+import java.lang.annotation.Target;
+
 public class device implements is_computer, is_mobile{
-    
+    protected target target;
+    protected String deviceType;
+    private String OS = System.getProperty("os.name").toLowerCase();
+    public boolean isaComputer(){
+        if(OS.indexOf("win") >= 0 || OS.indexOf("mac") >= 0 || OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0 ||  OS.indexOf("sunos") >= 0)
+            return true;
+        else 
+            return false;
+    }
+    public boolean isaMobile(){
+        if(isaComputer() == true)
+            return false;
+        else 
+            return true;
+    }
 }
