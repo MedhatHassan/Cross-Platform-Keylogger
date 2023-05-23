b0aa06c1f0215df2329036cea728f86434a849c1
Medhat Hassan <105995852+MedhatHassan@users.noreply.github.com>
2023-05-24 00:58:49 +0300

    Add files via upload

---
+++ Keylogger/SRC/file.java
@@ -0,0 +1,18 @@
+import java.sql.Date;
+
+public class file {
+    private String name;
+    private Date date;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public Date getdate()
+    {
+        return date;
+    }
+}
